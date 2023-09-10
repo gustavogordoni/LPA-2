91e8e5dfd4a79a76887d2f49b30448abb1cd133a
Gustavo Luiz Gordoni <gustavogordoni1@gmail.com>
2023-09-10 20:22:33 -0300

    Atualizando o projeto - 10/09

--- OuviAcess/src/br/com/ouviacess/dto/RequerimentosDTO.java
+++ OuviAcess/src/br/com/ouviacess/dto/RequerimentosDTO.java
@@ -1,7 +1,41 @@
 package br.com.ouviacess.dto;
 
 public class RequerimentosDTO {
-    private String resposta, titulo, tipo, situacao, data, descricao, cep, cidade, bairro, rua;
-    private int id_requerimento, id_usuario;
+    //private String resposta, titulo, tipo, situacao, data, descricao, cep, cidade, bairro, rua;
+    //private int id_requerimento, id_usuario;
     
+    private String resposta, situacao;
+    private int id_administrador, id_requerimento;    
+
+    public int getId_requerimento() {
+        return id_requerimento;
+    }
+
+    public void setId_requerimento(int id_requerimento) {
+        this.id_requerimento = id_requerimento;
+    }
+
+    public int getId_administrador() {
+        return id_administrador;
+    }
+
+    public void setId_administrador(int id_administrador) {
+        this.id_administrador = id_administrador;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+
+    public void setResposta(String resposta) {
+        this.resposta = resposta;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
 }
